cf7ccf14ebbff1c8b94a6963b5eadc321fec185b
auravadima <auravadima@gmail.com>
2017-10-09 22:09:32 +0700

    added users and user Me with my login and pass

--- src/main.java
+++ src/main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, " + args[0]);
+        //Collection of users
+        ArrayList<user> users = new ArrayList<user>();
+        users.add(new user("auravadima","rAAzhyGF"));
+        users.add(new user("vasya","qwerty"));
+        user Me = new user(args);
+        System.out.println("Hello, I'm " + Me.login + "\nMy password is " + Me.pass);
     }
 }
