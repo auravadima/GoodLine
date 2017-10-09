4fb60d9741b341953dcaf222c8688d299f0e81ef
auravadima <auravadima@gmail.com>
2017-10-09 23:46:08 +0700

    added help and combination checks

---
+++ src/cmd.java
@@ -0,0 +1,6 @@
+public class cmd {
+
+    public static void help(){
+        System.out.println("Это справка, которая вызывается, если число аргументов не равно двум или 4 или равно нулю");
+    }
+}
--- src/user.java
+++ src/user.java
@@ -6,11 +6,17 @@ public class user {
     String role;
 
     user(String ...  str){
-        this.login = str[0];
-        this.pass = str[1];
-        if(str.length == 4) {
+        if(str.length > 1) {
+            this.login = str[0];
+            this.pass = str[1];
+        }
+        else if(str.length == 4) {
             this.res = str[2];
             this.role = str[3];
         }
+        else{
+            cmd.help();
+        }
     }
+
 }
