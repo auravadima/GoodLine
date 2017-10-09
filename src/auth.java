52b74c434fd547e731280af501a57d17b6c4c228
auravadima <auravadima@gmail.com>
2017-10-09 23:46:43 +0700

    added error 2 and pass checking

--- src/auth.java
+++ src/auth.java
@@ -1,14 +1,22 @@
 public class auth {
 
-    public static void isUser(user us){
+    public static user isUser(user us){
 
         for(int i = 0; i < main.users.size(); i++){
             if(us.login.equals(main.users.get(i).login)) {
-                return;
+                return main.users.get(i);
             }
 
         }
         System.exit(1);
+        return us;
+    }
+
+    public static void rightPass(user us, user RegUs){
+        if (RegUs.pass.equals(us.pass)){
+
+        }
+        System.exit(2);
     }
 
 }
