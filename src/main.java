63b45a564bab8ada939b408313f2ed45224e7d71
auravadima <auravadima@gmail.com>
2017-10-10 20:04:40 +0700

    addes returned status and some conditions

--- src/main.java
+++ src/main.java
@@ -3,20 +3,21 @@ import org.apache.commons.cli.ParseException;
 import java.util.ArrayList;
 
 public class main {
-
+    public static int status = 0;
     public static ArrayList<user> users = new ArrayList<>();
 
-    public static void main(String[] args) throws ParseException {
-
+    public static void main(String[] args) throws ParseException, java.text.ParseException {
         cmd c = new cmd();
-        users.add(new user("auravadima","rAAzhyGF","A.B.C","WRITE"));
-        users.add(new user("vasya","qwerty","A.G.Y","READ"));
+        users.add(new user("auravadima","rAAzhyGF","A.B.C","WRITE","","",""));
+        users.add(new user("vasya","qwerty","A.G.Y","READ","","",""));
             String[] checkedArgs = c.Parse(args);
             user Me = new user(checkedArgs);
             user Reg = auth.isUser(Me);
-                auth.rightPass(Me, Reg);
-                cmd.checkArgs(args);
-                auth.access(Me, Reg);
-        System.exit(0);
+                    if(status == 0) auth.rightPass(Me,Reg);
+                    if(status == 0 && !checkedArgs[3].equals("")) auth.checkRole(checkedArgs);
+                    if(status == 0 && !checkedArgs[3].equals("")) auth.access(Me, Reg);
+                    if(status == 0 && !checkedArgs[6].equals("")) auth.checkDateVol(checkedArgs);
+            System.out.println(status);
+            System.exit(status);
     }
 }
