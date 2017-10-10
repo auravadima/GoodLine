dcc0728253c8eb7158a422295da0df45b58f9a55
auravadima <auravadima@gmail.com>
2017-10-10 16:59:11 +0700

    added ds de and vol to user class

--- src/user.java
+++ src/user.java
@@ -1,11 +1,18 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class user {
     
     String login;
     String pass;
     String res;
     String role;
+    Date ds;
+    Date de;
+    int vol;
 
-    user(String ...  str){
+    user(String ...  str) throws ParseException {
         if(str.length == 2) {
             this.login = str[0];
             this.pass = str[1];
@@ -13,6 +20,13 @@ public class user {
         if(str.length == 4){
             this.res = str[2];
             this.role = str[3];
+        }
+        if(str.length == 7){
+            SimpleDateFormat format = new SimpleDateFormat();
+            format.applyPattern("yyyy-MM-dd");
+            this.ds = format.parse(str[4]);
+            this.de = format.parse(str[5]);
+            this.vol = Integer.valueOf(str[6]);
         }
     }
 
