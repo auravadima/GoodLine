230f6bf0df5733a57e3f51f07435a1784fff89f2
auravadima <auravadima@gmail.com>
2017-10-15 02:39:31 +0700

    added class Authorization that save res and role

---
+++ src/Authorization.java
@@ -0,0 +1,11 @@
+
+public class Authorization {
+
+    String res;
+    String role;
+
+    public Authorization(String res, String role) {
+        this.res = res;
+        this.role = role;
+    }
+}
