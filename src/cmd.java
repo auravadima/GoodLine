0a6be433139f0caa070cdc2e12a05f37bd778753
auravadima <auravadima@gmail.com>
2017-10-15 02:41:39 +0700

    added methods

--- src/cmd.java
+++ src/cmd.java
@@ -1,69 +1,82 @@
 import org.apache.commons.cli.*;
 
-import java.text.SimpleDateFormat;
+import java.security.NoSuchAlgorithmException;
 
 public class cmd {
 
     private String[] args = null;
     public static Options option = new Options();
     CommandLineParser parser = new DefaultParser();
+    CommandLine cmd = null;
 
-
-    public String[] Parse(String[] args) throws ParseException {
-        this.args = args;
+    public cmd() {
         option.addOption("l", "login", true, "Enter login (required)");
-        option.addOption("p","pass",true, "Enter password (required)");
+        option.addOption("p", "pass", true, "Enter password (required)");
         option.addOption("res", true, "Enter res");
-        option.addOption("r","role",true, "Enter role (required if res argument exists");
+        option.addOption("r", "role", true, "Enter role (required if res argument exists");
         option.addOption("ds", true, "Enter start date");
-        option.addOption("de",true, "Enter end date (required if ds argument exists");
-        option.addOption("vol",true, "Enter vol (required if ds/de argument exists");
-        option.addOption("h","help",true,"Show help");
+        option.addOption("de", true, "Enter end date (required if ds argument exists");
+        option.addOption("vol", true, "Enter vol (required if ds/de argument exists");
+        option.addOption("h", "help", true, "Show help");
+    }
 
-        String[] returnArguments = new String[7];
-        CommandLine cmd = null;
-        boolean logPass = false;
-        boolean resRole = false;
-        try {
-            cmd = parser.parse(option, args);
-
-            if (cmd.hasOption("h")) {
-                help();
-                System.exit(-1);
-                return null;
-            }
-
-            if (cmd.hasOption("login") && cmd.hasOption("pass")) {
-                returnArguments[0] = cmd.getOptionValue("login");
-                returnArguments[1] = cmd.getOptionValue("pass");
-                logPass = true;
-            }
-
-            if (logPass && cmd.hasOption("res") && cmd.hasOption("role")) {
-                returnArguments[2] = cmd.getOptionValue("res");
-                returnArguments[3] = cmd.getOptionValue("role");
-                resRole = true;
-            }
-            else {
-                returnArguments[2] = "";
-                returnArguments[3] = "";
-            }
-
-            if(resRole && cmd.hasOption("ds") && cmd.hasOption("de") && cmd.hasOption("vol")) {
-                returnArguments[4] = cmd.getOptionValue("ds");
-                returnArguments[5] = cmd.getOptionValue("de");
-                returnArguments[6] = cmd.getOptionValue("vol");
-            }
-            else{
-                returnArguments[4] = "";
-                returnArguments[5] = "";
-                returnArguments[6] = "";
-            }
+    public boolean isAuthentication() {
+        if (cmd.hasOption("login") && cmd.hasOption("pass")) {
+            return true;
+        } else {
+            return false;
         }
-        catch (ParseException e) {
+    }
 
+    public boolean isAuthorization() {
+        if (isAuthentication() && cmd.hasOption("res") && cmd.hasOption("role")) {
+            return true;
+        } else {
+            return false;
         }
-        return returnArguments;
+    }
+
+    public boolean isAccounting() {
+        if (isAuthorization() && cmd.hasOption("ds") && cmd.hasOption("de") && cmd.hasOption("vol")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isHelp() {
+        if (cmd.hasOption("h")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public dataSet Parse(String[] args) throws ParseException, NoSuchAlgorithmException {
+        this.args = args;
+
+        dataSet set = new dataSet();
+        cmd = parser.parse(option, args);
+
+        if (isHelp()) {
+            help();
+            return null;
+        }
+
+        if (isAuthentication()) {
+            set.setLogin(cmd.getOptionValue("login"));
+            set.setPass(cmd.getOptionValue("pass"));
+        }
+
+        if (isAuthorization()) {
+            set.setRes(cmd.getOptionValue("res"), cmd.getOptionValue("role"));
+        }
+
+        if (isAccounting()) {
+            set.setInfo(cmd.getOptionValue("ds"), cmd.getOptionValue("de"), cmd.getOptionValue("vol"));
+        }
+        return set;
     }
 
     public static void help() {
