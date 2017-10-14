a9af9dd8cf50fa2068fce8e2b5c93342660c0dca
auravadima <auravadima@gmail.com>
2017-10-15 02:40:03 +0700

    added class Accounting that save ds de and vol

---
+++ src/Accounting.java
@@ -0,0 +1,13 @@
+public class Accounting {
+
+    String ds;
+    String de;
+    String vol;
+
+    public Accounting(String ds, String de, String vol) {
+        this.ds = ds;
+        this.de = de;
+        this.vol = vol;
+    }
+
+}
