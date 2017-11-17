0356d633afc50f8eae9f2e81a2da02aeb3f16cfc
auravadima <auravadima@gmail.com>
2017-11-17 11:29:15 +0700

    new class for logging

--- src/domain/LogOut.java
+++ src/domain/LogOut.java
@@ -1,4 +1,28 @@
 package domain;
 
-public class Logout {
+import java.util.logging.Logger;
+
+public class LogOut {
+    private final Logger logger = Logger.getLogger(LogOut.class.getName());
+
+    public void printLoginError(String login) {
+        logger.info(String.format("Login %s does not exist\n", login));
+    }
+
+    public void printPassError(String login, String password) {
+        logger.info(String.format("Password %s for user %s is incorrect\n", password, login));
+    }
+
+    public void printRoleError(String role) {
+        logger.info(String.format("Role %s is not defined\n", role));
+    }
+
+    public void printAccessError(String path, String role, String login) {
+        logger.info(String.format("Path %s with role %s for user %s not avaliable\n", path, role, login));
+    }
+
+    public void printAccountingError(String login) {
+        logger.info(String.format("User %s entered inccorect data(ds,de,vol)\n", login));
+    }
+
 }
