f51b28b590127c405d7b1a086b520d7c7be547e5
auravadima <auravadima@gmail.com>
2017-11-19 18:46:55 +0700

    cheanged classpath

--- BUILD.sh
+++ BUILD.sh
@@ -1,1 +1,1 @@
-javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar src/main/* src/domain/*
+javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar src/main/*.java src/domain/*.java
