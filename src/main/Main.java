b5fc51d94a7c528cb354dae78b47da6ddb96aef0
auravadima <auravadima@gmail.com>
2017-11-19 16:28:03 +0700

    удалены локальные user, теперь все проверки в базе данных

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,39 +2,12 @@ package main;
 
 import domain.LogOut;
 import domain.Roles;
-import domain.User;
-
-import java.util.ArrayList;
-
-import static domain.Roles.*;
 
 class Main {
 
     public static void main(String[] args) throws Exception {
 
         LogOut log = new LogOut();
-
-        ArrayList<User> users = new ArrayList<>();
-
-        users.add(new User("auravadima", "rAAzhyGF"));
-        users.add(new User("vasya", "qwerty"));
-        users.add(new User("jdoe", "sup3rpaZZ"));
-        users.add(new User("jrow", "Qweqrty12"));
-        users.add(new User("UWA", "HDP"));
-
-        for (User user : users) {
-            user.setSalt(Passwords.randSalt());
-            user.setPass(Passwords.getHash(user.getPass(), user.getSalt()));
-        }
-
-        users.get(0).addAccess("A.B", WRITE);
-        users.get(1).addAccess("A.K.Y", READ);
-        users.get(2).addAccess("a", READ);
-        users.get(2).addAccess("a.b", WRITE);
-        users.get(3).addAccess("a.b.c", EXECUTE);
-        users.get(3).addAccess("a.bc", EXECUTE);
-
-        users.get(0).addInf("2015-12-12", "2016-10-10", "354");
 
         CmdArgsParser cmdParser = new CmdArgsParser();
         DataSet userData = cmdParser.parse(args);
@@ -43,24 +16,25 @@ class Main {
             System.exit(0);
         }
 
-        User regUs = AuthService.findUser(userData.getLogin(), users);
+        Boolean usExist = AuthService.userExist(userData.getLogin());
 
         if (userData.hasAuthenticationData()) {
-            if (regUs == null) {
+            if (!usExist) {
                 log.printLoginError(userData.getLogin());
                 System.exit(1);
             }
-            if (!AuthService.isRightPass(userData.getPass(), regUs)) {
-                log.printPassError(userData.getPass(), regUs.getLogin());
+            if (!AuthService.isRightPass(userData.getPass(), userData.getLogin())) {
+                log.printPassError(userData.getLogin(), userData.getPass());
                 System.exit(2);
             }
         }
+
         if (userData.hasAuthorizationData()) {
             if (!Roles.isDefined(userData.getRole())) {
                 log.printRoleError(userData.getRole());
                 System.exit(3);
             }
-            if (!AuthService.hasAccess(userData.getRes(), userData.getRole(), regUs)) {
+            if (!AuthService.hasAccess(userData.getRes(), userData.getRole(), userData.getLogin())) {
                 log.printAccessError(userData.getRes(), userData.getRole(), userData.getLogin());
                 System.exit(4);
             }
