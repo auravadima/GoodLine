d75ef0c7cda3dadda20ba48223123b54910ef23e
auravadima <auravadima@gmail.com>
2017-11-20 14:31:22 +0700

    added close()

--- src/main/DB.java
+++ src/main/DB.java
@@ -5,36 +5,29 @@ import java.util.ArrayList;
 
 class DB {
 
-    /**
-     * Возвразает Connection с базой данных
-     */
     Connection getConn() throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
         return DriverManager
                 .getConnection("jdbc:h2:file:~/database", "auravadima", "rAAzhyGF1");
     }
 
-    /**
-     * Возвращает ResultSet sql запроса
-     */
     ResultSet getRS(String sqlQuery, Connection conn) throws SQLException {
         Statement st = conn.createStatement();
         return st.executeQuery(sqlQuery);
     }
 
-    /**
-     * Возвращает коллекцию типа String, полученную из sql запроса
-     */
     ArrayList<String> getResultArray(ResultSet rs, String column) throws SQLException {
         ArrayList<String> result = new ArrayList<>();
         while (rs.next()) {
             result.add(rs.getString(column));
         }
+        rs.close();
         return result;
     }
 
-    String createQuery(String table, String row) throws SQLException, ClassNotFoundException {
+    String createQuery(String table, String row) {
         return String.format("SELECT * FROM %s WHERE LOGIN='%s'", table, row);
     }
+
 }
 
