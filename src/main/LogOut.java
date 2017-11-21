2b85c84c6d5d556b3a7747438b952a98160be1e5
auravadima <auravadima@gmail.com>
2017-11-21 19:26:11 +0700

    replaced

--- src/main/LogOut.java
+++ src/main/LogOut.java
@@ -1,28 +1,29 @@
-package domain;
+package main;
+
 
 import java.util.logging.Logger;
 
-public class LogOut {
+class LogOut {
 
     private final Logger logger = Logger.getLogger(LogOut.class.getName());
 
-    public void printLoginError(String login) {
+    void printLoginError(String login) {
         logger.info(String.format("Login %s does not exist", login));
     }
 
-    public void printPassError(String login, String password) {
+    void printPassError(String login, String password) {
         logger.info(String.format("Password %s for user %s is incorrect", password, login));
     }
 
-    public void printRoleError(String role) {
+    void printRoleError(String role) {
         logger.info(String.format("Role %s is not defined", role));
     }
 
-    public void printAccessError(String path, String role, String login) {
+    void printAccessError(String path, String role, String login) {
         logger.info(String.format("Path %s with role %s for user %s not avaliable", path, role, login));
     }
 
-    public void printAccountingError(String login) {
+    void printAccountingError(String login) {
         logger.info(String.format("User %s entered inccorect data(ds,de,vol)", login));
     }
 
