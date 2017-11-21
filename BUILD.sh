473cf2e538c02bad78519b51573ff2bcb9f4cd76
auravadima <auravadima@gmail.com>
2017-11-21 19:26:22 +0700

    now for win too

--- BUILD.sh
+++ BUILD.sh
@@ -1,1 +1,5 @@
-javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar src/main/*.java src/domain/*.java
+if [[ "$OSTYPE" == "win32" ]]; then
+        javac -d bin -cp libs/commons-cli-1.4.jar;libs/flyway-core-4.2.0.jar src/main/*.java src/domain/*.java
+else
+        javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar src/main/*.java src/domain/*.java
+fi
