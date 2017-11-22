7773749d78b08b3df4be0f963206f5be3ad6a9b7
auravadima <auravadima@gmail.com>
2017-11-22 20:03:12 +0700

    added libs to classpath

--- BUILD.sh
+++ BUILD.sh
@@ -1,5 +1,5 @@
 if [[ "$OSTYPE" == "win32" ]]; then
-        javac -d bin -cp libs/commons-cli-1.4.jar;libs/flyway-core-4.2.0.jar src/main/*.java src/domain/*.java
+        javac -d bin -cp libs/commons-cli-1.4.jar;libs/flyway-core-4.2.0.jar;libs/log4j-api-2.9.1.jar;libs/log4j-core-2.9.1c.jar src/main/*.java src/domain/*.java
 else
-        javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar src/main/*.java src/domain/*.java
+        javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar:libs/log4j-api-2.9.1.jar:libs/log4j-core-2.9.1c.jar src/main/*.java src/domain/*.java
 fi
