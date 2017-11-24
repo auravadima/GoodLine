f5bf0ef34849dbdbe73a7ae072271f07c1365b0c
auravadima <auravadima@gmail.com>
2017-11-24 15:20:17 +0700

    fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,60 +13,55 @@ class Main {
 
         final Logger logger = LogManager.getRootLogger();
         AuthService authService = new AuthService();
-        DB db;
-        try {
-            db = new DB();
-            try (Connection conn = db.getConn()) {
-                db.migrate();
+        DB db = new DB();
+        db.migrate();
+        db.connect();
+        try (Connection conn = db.getConn()) {
 
-                CmdArgsParser cmdParser = new CmdArgsParser();
-                DataSet userData = cmdParser.parse(args);
-                if (userData.getLogin() == null || userData.getHelp()) {
-                    cmdParser.help();
+            CmdArgsParser cmdParser = new CmdArgsParser();
+            DataSet userData = cmdParser.parse(args);
+            if (userData.getLogin() == null || userData.getHelp()) {
+                cmdParser.help();
+                db.getConn().close();
+                System.exit(0);
+            }
+
+            Boolean usExist = authService.userExist(db, userData.getLogin());
+
+            if (userData.hasAuthenticationData()) {
+                if (!usExist) {
+                    logger.info(String.format("Login %s does not exist", userData.getLogin()));
                     db.getConn().close();
-                    System.exit(0);
+                    System.exit(1);
                 }
-
-                Boolean usExist = authService.userExist(db, userData.getLogin());
-
-                if (userData.hasAuthenticationData()) {
-                    if (!usExist) {
-                        logger.info(String.format("Login %s does not exist", userData.getLogin()));
-                        db.getConn().close();
-                        System.exit(1);
-                    }
-                    if (!authService.isRightPass(db, userData.getPass(), userData.getLogin())) {
-                        logger.info(String.format("Password %s for user %s is incorrect", userData.getPass(), userData.getLogin()));
-                        db.getConn().close();
-                        System.exit(2);
-                    }
-                }
-
-                if (userData.hasAuthorizationData()) {
-                    if (!Roles.isDefined(userData.getRole())) {
-                        logger.info(String.format("Role %s is not defined", userData.getRole()));
-                        db.getConn().close();
-                        System.exit(3);
-                    }
-                    if (!authService.hasAccess(db, userData.getRes(), userData.getRole(), userData.getLogin())) {
-                        logger.info(String.format("Path %s with role %s for user %s not avaliable",
-                                userData.getRes(), userData.getRole(), userData.getLogin()));
-                        db.getConn().close();
-                        System.exit(4);
-                    }
-                }
-                if (userData.hasAccountingData()
-                        && (!authService.isDateValid(userData.getDs())
-                        || !authService.isDateValid(userData.getDe())
-                        || !authService.isVolValid(userData.getVol()))) {
-                    logger.info(String.format("User %s entered inccorect data(ds,de,vol)", userData.getLogin()));
+                if (!authService.isRightPass(db, userData.getPass(), userData.getLogin())) {
+                    logger.info(String.format("Password %s for user %s is incorrect", userData.getPass(), userData.getLogin()));
                     db.getConn().close();
-                    System.exit(5);
+                    System.exit(2);
                 }
             }
-        } catch (Exception e) {
-            logger.info("Troubles with database connection");
-            System.exit(255);
+
+            if (userData.hasAuthorizationData()) {
+                if (!Roles.isDefined(userData.getRole())) {
+                    logger.info(String.format("Role %s is not defined", userData.getRole()));
+                    db.getConn().close();
+                    System.exit(3);
+                }
+                if (!authService.hasAccess(db, userData.getRes(), userData.getRole(), userData.getLogin())) {
+                    logger.info(String.format("Path %s with role %s for user %s not avaliable",
+                            userData.getRes(), userData.getRole(), userData.getLogin()));
+                    db.getConn().close();
+                    System.exit(4);
+                }
+            }
+            if (userData.hasAccountingData()
+                    && (!authService.isDateValid(userData.getDs())
+                    || !authService.isDateValid(userData.getDe())
+                    || !authService.isVolValid(userData.getVol()))) {
+                logger.info(String.format("User %s entered inccorect data(ds,de,vol)", userData.getLogin()));
+                db.getConn().close();
+                System.exit(5);
+            }
         }
         System.exit(0);
     }
