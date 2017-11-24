7e899e962f2a39a78b2c2a98d2107f2ad3a46206
auravadima <auravadima@gmail.com>
2017-11-24 13:32:59 +0700

    changed to jar

--- RUN.sh
+++ RUN.sh
@@ -1,5 +1,1 @@
-if [[ "$OSTYPE" == "win32" ]]; then
-        java -Dlog4j.configurationFile=log4j.properties -classpath bin;libs/*;src main.Main $1
-else
-        java -Dlog4j.configurationFile=log4j.properties -classpath bin:libs/*:src main.Main $1
-fi
+java  -jar run.jar -l auravadima main.Main $@
