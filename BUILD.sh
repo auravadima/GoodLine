46bd36b217a683219c516b093afcd72a9f6c31e2
auravadima <auravadima@gmail.com>
2017-11-24 13:32:39 +0700

    jar compile

--- BUILD.sh
+++ BUILD.sh
@@ -1,5 +1,15 @@
 if [[ "$OSTYPE" == "win32" ]]; then
-        javac -d bin -cp libs/commons-cli-1.4.jar;libs/flyway-core-4.2.0.jar;libs/log4j-api-2.9.1.jar;libs/log4j-core-2.9.1c.jar src/main/*.java src/domain/*.java
+        javac -d jarfiles -cp libs/commons-cli-1.4.jar;libs/flyway-core-4.2.0.jar;libs/log4j-api-2.9.1.jar;libs/log4j-core-2.9.1c.jar src/services/* src/main/*.java src/domain/*.java
 else
-        javac -d bin -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar:libs/log4j-api-2.9.1.jar:libs/log4j-core-2.9.1c.jar src/main/*.java src/domain/*.java
+        javac -d jarfiles -cp libs/commons-cli-1.4.jar:libs/flyway-core-4.2.0.jar:libs/log4j-api-2.9.1.jar:libs/log4j-core-2.9.1c.jar src/services/* src/main/*.java src/domain/*.java
 fi
+mkdir jarfiles
+cd jarfiles
+jar xf ../libs/commons-cli-1.4.jar 
+jar xf ../libs/h2-1.4.196.jar 
+jar xf ../libs/log4j-core-2.9.1.jar 
+jar xf ../libs/flyway-core-4.2.0.jar 
+jar xf ../libs/log4j-api-2.9.1.jar 
+jar cfm run.jar ../META-INF/MANIFEST.MF main domain services ../db META-INF org ../log4j2.xml ../connection.properties 
+mv run.jar ../run.jar
+
