eac034ff9d76154f3de1803a1bada55ee21d8b6c
auravadima <auravadima@gmail.com>
2017-11-29 01:01:48 +0700

    added status check

--- src/main/Main.java
+++ src/main/Main.java
@@ -32,24 +32,24 @@ class Main {
                         logger.info(String.format("Login %s does not exist", userData.getLogin()));
                         status = 1;
                     }
-                    if (!authService.isRightPass(userData.getPass(), userData.getLogin()) && status == 0) {
+                    if (status == 0 && !authService.isRightPass(userData.getPass(), userData.getLogin())) {
                         logger.info(String.format("Password %s for user %s is incorrect", userData.getPass(), userData.getLogin()));
                         status = 2;
                     }
                 }
 
-                if (userData.hasAuthorizationData() && status == 0) {
+                if (status == 0 && userData.hasAuthorizationData()) {
                     if (!Roles.isDefined(userData.getRole())) {
                         logger.info(String.format("Role %s is not defined", userData.getRole()));
                         status = 3;
                     }
-                    if (!authService.hasAccess(userData.getRes(), userData.getRole(), userData.getLogin()) && status == 0) {
+                    if (status == 0 && !authService.hasAccess(userData.getRes(), userData.getRole(), userData.getLogin())) {
                         logger.info(String.format("Path %s with role %s for user %s not avaliable",
                                 userData.getRes(), userData.getRole(), userData.getLogin()));
                         status = 4;
                     }
                 }
-                if (userData.hasAccountingData() && status == 0
+                if (status == 0 && userData.hasAccountingData()
                         && (!authService.isDateValid(userData.getDs())
                         || !authService.isDateValid(userData.getDe())
                         || !authService.isVolValid(userData.getVol()))) {
