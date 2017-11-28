1826b63aa4dca08e34674850d503aa19acde4556
auravadima <auravadima@gmail.com>
2017-11-28 23:49:01 +0700

    no pwning JVM before conn.close()

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,8 @@ class Main {
 
     public static void main(String[] args) throws Exception {
 
+        int status = 0;
+
         final Logger logger = LogManager.getRootLogger();
         AuthService authService = new AuthService();
         try (Connection conn = new DB().getConn()) {
@@ -19,42 +21,44 @@ class Main {
             DataSet userData = cmdParser.parse(args);
             if (userData.getLogin() == null || userData.getHelp()) {
                 cmdParser.help();
-                System.exit(0);
             }
+            else {
 
-            Boolean usExist = authService.userExist(conn, userData.getLogin());
+                Boolean usExist = authService.userExist(conn, userData.getLogin());
 
-            if (userData.hasAuthenticationData()) {
-                if (!usExist) {
-                    logger.info(String.format("Login %s does not exist", userData.getLogin()));
-                    System.exit(1);
+                if (userData.hasAuthenticationData()) {
+                    if (!usExist) {
+                        logger.info(String.format("Login %s does not exist", userData.getLogin()));
+                        status = 1;
+                    }
+                    if (!authService.isRightPass(conn, userData.getPass(), userData.getLogin()) && status == 0) {
+                        logger.info(String.format("Password %s for user %s is incorrect", userData.getPass(), userData.getLogin()));
+                        status = 2;
+                    }
                 }
-                if (!authService.isRightPass(conn, userData.getPass(), userData.getLogin())) {
-                    logger.info(String.format("Password %s for user %s is incorrect", userData.getPass(), userData.getLogin()));
-                    System.exit(2);
-                }
-            }
 
-            if (userData.hasAuthorizationData()) {
-                if (!Roles.isDefined(userData.getRole())) {
-                    logger.info(String.format("Role %s is not defined", userData.getRole()));
-                    System.exit(3);
+                if (userData.hasAuthorizationData() && status == 0) {
+                    if (!Roles.isDefined(userData.getRole())) {
+                        logger.info(String.format("Role %s is not defined", userData.getRole()));
+                        status = 3;
+                    }
+                    if (!authService.hasAccess(conn, userData.getRes(), userData.getRole(), userData.getLogin()) && status == 0) {
+                        logger.info(String.format("Path %s with role %s for user %s not avaliable",
+                                userData.getRes(), userData.getRole(), userData.getLogin()));
+                        status = 4;
+                    }
                 }
-                if (!authService.hasAccess(conn, userData.getRes(), userData.getRole(), userData.getLogin())) {
-                    logger.info(String.format("Path %s with role %s for user %s not avaliable",
-                            userData.getRes(), userData.getRole(), userData.getLogin()));
-                    System.exit(4);
+                if (userData.hasAccountingData() && status == 0
+                        && (!authService.isDateValid(userData.getDs())
+                        || !authService.isDateValid(userData.getDe())
+                        || !authService.isVolValid(userData.getVol()))) {
+                    logger.info(String.format("User %s entered inccorect data(ds,de,vol)", userData.getLogin()));
+                    status = 5;
                 }
             }
-            if (userData.hasAccountingData()
-                    && (!authService.isDateValid(userData.getDs())
-                    || !authService.isDateValid(userData.getDe())
-                    || !authService.isVolValid(userData.getVol()))) {
-                logger.info(String.format("User %s entered inccorect data(ds,de,vol)", userData.getLogin()));
-                System.exit(5);
-            }
-            System.exit(0);
+        }catch (Exception e){
+            status = 255;
         }
-        System.exit(255);
+        System.exit(status);
     }
 }
